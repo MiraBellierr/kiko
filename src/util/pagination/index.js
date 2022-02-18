c5d7f3947452e680f4b3e450fb7ca4672bacb1b3
mira <75848@siswa.unimas.my>
2022-02-19 00:47:45 +0800

    add pagination

---
+++ src/util/pagination/index.js
@@ -0,0 +1,9 @@
+const InteractionPaginate = require("./InteractionPaginate");
+const { Interaction } = require("discord.js");
+
+Interaction.prototype.paginate = async function(pages, options, emojis) {
+	const paginated = new InteractionPaginate(this.client, this, pages, options, emojis);
+	await paginated.init();
+};
+
+module.exports = { InteractionPaginate };
