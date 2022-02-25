4415d7364b2d5421dede9bff4e4304dc8c8dc703
mira <75848@siswa.unimas.my>
2022-02-25 12:19:51 +0800

    client id check for testing purpose

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,12 +1,10 @@
-// const registerInteraction = require("../handlers/registerCommand");
-const registerGuildCommand = require("../handlers/registerGuildCommand");
 const registerCommand = require("../handlers/registerCommand");
+const registerGuildCommand = require("../handlers/registerGuildCommand");
 
 module.exports = async client => {
 	client.user.setPresence({ activities: [{ name: `${client.guilds.cache.size.toLocaleString()} servers ✨ | Slash command!`, type: "WATCHING" }], status: "idle" });
 	console.log(`Hi, ${client.user.username} is now online!`);
 
-	// registerInteraction(client);
-	registerGuildCommand(client, "873441703330185247");
-	registerCommand(client);
+	if (client.user.id === "938774709762408490") registerCommand(client);
+	else registerGuildCommand(client, "873441703330185247");
 };
