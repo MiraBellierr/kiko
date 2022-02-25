a21ed78c2d4eb599ee31b5314929af32b6d8aba2
mira <75848@siswa.unimas.my>
2022-02-25 09:24:15 +0800

    fix whois

--- src/commands/[✨] utility/whois.js
+++ src/commands/[✨] utility/whois.js
@@ -5,6 +5,7 @@ const Pagination = require("../../util/pagination");
 module.exports = {
 	name: "whois",
 	description: "Returns information about a user.",
+	category: "[✨] utility",
 	options: [{
 		name: "user",
 		description: "The user to get information about.",
