b04d66c6c132b817227bd002f836bae65a3a43e9
mira <75848@siswa.unimas.my>
2022-03-02 15:04:36 +0800

    add balance command

---
+++ src/commands/economy/balance.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const { Balance } = require("../../database/schemes/balance");
+const functions = require("../../util/functions");
+
+module.exports = {
+	name: "balance",
+	description: "check user balance",
+	category: "economy",
+	options: [{
+		name: "user",
+		description: "choose a user",
+		type: 6
+	}],
+	run: async (client, interaction) => {
+		const user = interaction.options.getUser("user") || interaction.user;
+		const balance  = await functions.getUserData(Balance(), user);
+		const paws = balance.get("balance");
+
+		const embed = new Discord.MessageEmbed()
+			.setAuthor({ name: `${user.username}'s balance`})
+			.setThumbnail(user.displayAvatarURL({ dynamic: true }))
+			.setDescription(`**Balance:** :paw: ${paws}`)
+			.setColor("#CD1C6C")
+			.setFooter({ text: "*insert patreon link here*", iconURL: client.user.displayAvatarURL() });
+
+		interaction.reply({ embeds: [embed] });
+	}
+};
---
+++ src/database/schemes/balance.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("database", "user", "password", {
+	host: "localhost",
+	dialect: "sqlite",
+	logging: console.log,
+	storage: "./src/database/database.sqlite",
+});
+
+const Balance = function() {
+	const BalanceScheme = sequelize.define("balance", {
+		userid: {
+			type: Sequelize.STRING,
+			unique: true,
+			allowNull: false,
+		},
+		balance: {
+			type: Sequelize.INTEGER,
+			defaultValue: 0
+		}
+	});
+
+	BalanceScheme.sync();
+
+	return BalanceScheme;
+};
+
+module.exports = { Balance };
