58dbb6b7132e6f63439d6aab9b19dfc22517c48a
mirabellier <darkfur001v@gmail.com>
2022-03-26 21:21:36 +0800

    apply LICENSE

--- src/commands/💰 : economy/work.js
+++ src/commands/💰 : economy/work.js
@@ -1,3 +1,19 @@
+/**
+ *    Copyright 2022 MiraBellierr
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 const functions = require("../../util/functions");
 const { Balance, Cooldown } = require("../../database/schemes");
 const words = ["I'm working for money.", "Today, I want to find a job.", "Time to go working!", "Let me take a break for a while", "Oh no, I'm late for work!", "Boss, I'm done."];
